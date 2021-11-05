50abe7f50751f72046ce33f9ec20390eababfeae
kelvin <kelvin.vitone@gmail.com>
2021-11-05 12:18:06 +0300

    migrated auth bloc to 8.*  from mapEventToState to emiter

--- lib/application/auth/auth_bloc.dart
+++ lib/application/auth/auth_bloc.dart
@@ -13,25 +13,55 @@ part 'auth_bloc.freezed.dart';
 
 @injectable
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc(this._authFacade) : super(const AuthState.initial());
+  AuthBloc(this._authFacade) : super(const AuthState.initial()) {
+    on<AuthCheckRequested>(_authCheckRequested);
+    on<SignedOut>(_signOut);
+  }
+
   final IAuthFacade _authFacade;
 
-  @override
-  Stream<AuthState> mapEventToState(
-    AuthEvent event,
-  ) async* {
-    yield* event.map(
-      authCheckRequested: (e) async* {
-        final userOption = await _authFacade.getSignedInUser();
-        yield userOption.fold(
-          () => const AuthState.unauthenticated(),
-          (_) => const AuthState.authenticated(),
-        );
-      },
-      signedOut: (e) async* {
-        await _authFacade.signOut();
-        yield const AuthState.unauthenticated();
-      },
+  Future _authCheckRequested(
+    AuthCheckRequested event,
+    Emitter<AuthState> emit,
+  ) async {
+    final userOption = await _authFacade.getSignedInUser();
+    emit(
+      userOption.fold(
+        () => const AuthState.unauthenticated(),
+        (_) => const AuthState.authenticated(),
+      ),
     );
   }
+
+  Future _signOut(
+    SignedOut event,
+    Emitter<AuthState> emit,
+  ) async {
+    await _authFacade.signOut();
+
+    emit(const AuthState.unauthenticated());
+  }
 }
+// class AuthBloc extends Bloc<AuthEvent, AuthState> {
+//   AuthBloc(this._authFacade) : super(const AuthState.initial());
+//   final IAuthFacade _authFacade;
+
+//   @override
+//   Stream<AuthState> mapEventToState(
+//     AuthEvent event,
+//   ) async* {
+//     yield* event.map(
+//       authCheckRequested: (e) async* {
+//         final userOption = await _authFacade.getSignedInUser();
+//         yield userOption.fold(
+//           () => const AuthState.unauthenticated(),
+//           (_) => const AuthState.authenticated(),
+//         );
+//       },
+//       signedOut: (e) async* {
+//         await _authFacade.signOut();
+//         yield const AuthState.unauthenticated();
+//       },
+//     );
+//   }
+// }
